16ae5661a58d5541f2d5a51c868feb23d53e9c2e
Guilherme Fernandes <guilhermelff@id.uff.br>
2024-02-04 20:57:30 -0300

    category fixes

--- django/Restaurant/RestaurantAPI/views.py
+++ django/Restaurant/RestaurantAPI/views.py
@@ -1,7 +1,10 @@
 
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from.models import MenuItem, Category
 from.serializers import MenuItemSerializer, CategorySerializer
+
 
 
 # Create your views here.
