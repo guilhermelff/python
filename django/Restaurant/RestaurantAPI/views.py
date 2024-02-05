1a3a574544b60de8ff8f3216db5a6dbb0ce7ecd9
Guilherme Fernandes <guilhermelff@id.uff.br>
2024-02-04 23:22:08 -0300

    search endpoint

--- django/Restaurant/RestaurantAPI/views.py
+++ django/Restaurant/RestaurantAPI/views.py
@@ -15,6 +15,7 @@ def menu_items(request):
         items = MenuItem.objects.select_related('category').all()
         category_name = request.query_params.get('category')
         to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
         if category_name:
             items = items.filter(category__title=category_name)
         if to_price:
