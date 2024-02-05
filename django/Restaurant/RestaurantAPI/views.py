cfebbc817c3347b7909af2d03e4e1b6a952942ac
Guilherme Fernandes <guilhermelff@id.uff.br>
2024-02-04 23:31:37 -0300

    sort results

--- django/Restaurant/RestaurantAPI/views.py
+++ django/Restaurant/RestaurantAPI/views.py
@@ -16,12 +16,16 @@ def menu_items(request):
         category_name = request.query_params.get('category')
         to_price = request.query_params.get('to_price')
         search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
         if search:
             items = items.filter(title__icontains=search)
         if category_name:
             items = items.filter(category__title=category_name)
         if to_price:
             items = items.filter(price__lte=to_price)
+        if ordering:
+            ordering_fields = ordering.split(',')
+            items = items.order_by(*ordering_fields)
         serialized_item = MenuItemSerializer(items, many=True)
         return Response(serialized_item.data)
     elif request.method == 'POST':
