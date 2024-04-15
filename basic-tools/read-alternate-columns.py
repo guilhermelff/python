7fbbff2eb76c340bd418e9b98e032c22525b5f6c
Guilherme Fernandes <contatoguilhermelff@gmail.com>
2024-04-15 10:25:23 -0300

    Create read-alternate-columns.py

---
+++ basic-tools/read-alternate-columns.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+path = r"data\teste-09042024-152239_-_P12-ContasReceber-H3_Compensacoes_Estorno_de_nota_fiscal_ou_nota_de_credito.xlsx"
+
+def le_colunas_alternadas(path, coluna1, coluna2)
