99eb941e698eec061f6135397a1e4f496c1ccf4a
Guilherme Fernandes <contatoguilhermelff@gmail.com>
2024-04-15 10:43:09 -0300

    Update read-alternate-columns.py

--- basic-tools/read-alternate-columns.py
+++ basic-tools/read-alternate-columns.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-path = r"data\teste-09042024-152239_-_P12-ContasReceber-H3_Compensacoes_Estorno_de_nota_fiscal_ou_nota_de_credito.xlsx"
+def le_colunas_alternadas(path:str, colunas:list) -> list:
+    df = pd.read_excel(path)
+    valoresAlternados = []
+    
+    for index, row in df.iterrows():
+        for coluna in colunas:
+            valoresAlternados.append(row[coluna])
+    
+    return valoresAlternados
 
-def le_colunas_alternadas(path, coluna1, coluna2)
+path = r"data\teste-09042024-152239_-_P12-ContasReceber-H3_Compensacoes_Estorno_de_nota_fiscal_ou_nota_de_credito.xlsx"
+colunas = ["Nº Documento  Fatura no SAP", "Nº Documento Adiantamento ou Nota de crédito no SAP"]
+le_colunas_alternadas(path, colunas)
