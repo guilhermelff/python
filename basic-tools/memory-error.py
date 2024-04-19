065b87e8c4d283c6e69374bfbe98cc071828ed59
Guilherme Fernandes <contatoguilhermelff@gmail.com>
2024-04-19 08:48:30 -0300

    Update memory-error.py

--- basic-tools/memory-error.py
+++ basic-tools/memory-error.py
@@ -2,20 +2,41 @@ import gc
 
 keep = True
 lista = []
-list2 = []
+lista2 = []
 
-list3 = []
-list4 = []
+lista3 = []
+lista4 = []
 
-while len(list2) < 30000000:
-    lista.append("oi")
-    list2.extend(lista)
-    print(len(list2))
+
+while len(lista2) < 50000000:
+    try:
+        lista.append("oi")
+        lista2.extend(lista)
+        print(len(lista2))
+    except MemoryError:
+        print("MEMORIA EXCEDIDA")  
+        keep = False
+        break
+
+del lista
+del lista2
+
+while len(lista4) < 60000000:
+    try:
+        lista3.append("oi")
+        lista4.extend(lista3)
+        print(len(lista4))
+    except MemoryError:
+        print("MEMORIA EXCEDIDA")
+        keep = False
+        break
     
-del list2
+del lista3
+del lista4
 
-while len(list4) < 30000000:
-    list3.append("oi")
-    list4.extend(list3)
-    print(gc.garbage)
-    print(len(list4))
+print("\n")
+print("STATUS DO GC")
+print(gc.get_stats())
+print("\n")
+print("LIMITES DE COLETA DO GC")
+print(gc.get_threshold())
